293e1467574a1f5d2baf84b95049a5af575d30ff
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-04-08 07:24:46 -0300

    função de abstração

--- Rpg.js
+++ Rpg.js
@@ -1,1 +1,9 @@
-hellow word
+function characterCard (){
+    this.Name = Name;
+    this.Type = Type;
+    this.Damage = Damage;
+    this.Vitality = Vitality;
+    this.Mana = Mana;
+    this.Stamina = Stamina;
+}
+
